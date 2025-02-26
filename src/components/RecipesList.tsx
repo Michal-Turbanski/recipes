9fe105667aa8cceb619163050061e87c7d663b08
Michal-Turbanski <michalturbanski@gmail.com>
2025-02-26 13:06:26 +0100

    feat: add recipe component

--- src/components/RecipesList.tsx
+++ src/components/RecipesList.tsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import {RecipeType} from "../types/recipe.type.ts";
+import Recipe from "./Recipe.tsx";
 
 function RecipesList() {
 
@@ -19,23 +20,7 @@ function RecipesList() {
     return (
         <>
             {recipes.map((recipe: RecipeType) => (
-                <div key={recipe.id}>
-                    <h1 className="text-4xl">{recipe.name}</h1>
-                    <h2 className="text-2xl">Składniki:</h2>
-                    <ul>
-                        {recipe.ingredients.map((ingredient) => (
-                            <li key={ingredient.id}>
-                                {ingredient.amount} {ingredient.unit} {ingredient.name}
-                            </li>
-                        ))}
-                    </ul>
-                    <h2 className="text-2xl">Przygotowanie: </h2>
-                    <ol>
-                        {recipe.description.map((step, index) => (
-                            <li key={index}>{step}</li>
-                        ))}
-                    </ol>
-                </div>
+                <Recipe key={recipe.id} recipe={recipe} />
             ))}
         </>
     );
