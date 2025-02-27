3cb3825336fc254e4f72221311d2c44fbc649343
Michal-Turbanski <michalturbanski@gmail.com>
2025-02-27 13:44:22 +0100

    feat: add active navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,19 @@
-import {Link} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
+
+    const linkClass = ({isActive}: {isActive: boolean}) =>
+        isActive ?
+            'px-6 py-3 rounded-lg hover:bg-gray-600 transition-all bg-gray-600' :
+            'px-6 py-3 rounded-lg hover:bg-gray-600 transition-all';
+
     return (
         <nav className="bg-gray-800 text-white p-4">
             <div className="container mx-auto flex justify-between items-center">
-                <Link to="/" className="text-xl font-bold">Przepisy kulinarne</Link>
+                <NavLink to="/" className="text-xl font-bold">Przepisy kulinarne</NavLink>
                 <ul className="flex flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-4 items-center">
-                    <li><Link to="/" className="px-6 py-3 rounded-lg hover:bg-gray-600 transition-all">Przepisy</Link></li>
-                    <li><Link to="/add-recipe" className="px-6 py-3 rounded-lg hover:bg-gray-600 transition-all">Dodaj przepis</Link></li>
+                    <li><NavLink to="/" className={linkClass}>Przepisy</NavLink></li>
+                    <li><NavLink to="/add-recipe" className={linkClass}>Dodaj przepis</NavLink></li>
                 </ul>
             </div>
         </nav>
