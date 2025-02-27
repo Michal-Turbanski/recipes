3036037bc8c09d8e30a44a60860cf8025e2480c2
Michal-Turbanski <michalturbanski@gmail.com>
2025-02-27 13:11:47 +0100

    feat: add home page and install react-router-dom

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar.tsx";
+import RecipesList from "../components/RecipesList.tsx";
+
+function HomePage() {
+    return (
+        <>
+            <Navbar/>
+            <RecipesList/>
+        </>
+    );
+}
+
+export default HomePage;
