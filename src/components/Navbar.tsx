8a0f7faa2323f1faa4cc8c41cb5ea3daf2748f07
Michal-Turbanski <michalturbanski@gmail.com>
2025-02-27 12:48:15 +0100

    feat: remove main page from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,8 @@ function Navbar() {
             <div className="container mx-auto flex justify-between items-center">
                 <a href="#" className="text-xl font-bold">Przepisy kulinarne</a>
                 <ul className="flex flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-4 items-center">
-                    <li><a href="#" className="px-6 py-3 rounded-lg hover:bg-gray-600 transition-all">Strona główna</a></li>
                     <li><a href="#" className="px-6 py-3 rounded-lg hover:bg-gray-600 transition-all">Przepisy</a></li>
-                    <li><a href="#" className="px-6 py-3 rounded-lg hover:bg-gray-600 transition-all">O stronie</a></li>
+                    <li><a href="#" className="px-6 py-3 rounded-lg hover:bg-gray-600 transition-all">Dodaj przepis</a></li>
                 </ul>
             </div>
         </nav>
