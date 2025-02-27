6d38b3a3c6e652986c4fd9872cc9e94e4847830e
Michal-Turbanski <michalturbanski@gmail.com>
2025-02-27 13:28:53 +0100

    feat: add navbar to 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar.tsx";
 
 const NotFoundPage = () => {
     return (
-        <section className="text-center flex flex-col justify-center items-center h-96">
-            <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
-            <p className="text-xl mb-5">Strona nie istnieje</p>
-            <Link
-                to="/"
-                className="bg-amber-200 hover:bg-amber-300 rounded-lg px-3 py-2 mt-4"
-            >Wróć</Link
-            >
-        </section>
+        <>
+            <Navbar />
+            <section className="text-center flex flex-col justify-center items-center h-96">
+                <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
+                <p className="text-xl mb-5">Strona nie istnieje</p>
+                <Link
+                    to="/"
+                    className="bg-amber-200 hover:bg-amber-300 rounded-lg px-3 py-2 mt-4"
+                >Wróć</Link
+                >
+            </section>
+        </>
     );
 };
 
